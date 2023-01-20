07558d57d22bd827f3d8292dde52ca89967d6dfa
jax-parker <parkyjax711@gmail.com>
2023-01-20 17:34:28 +0000

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,11 +21,11 @@ document.addEventListener('DOMContentLoaded', function() {
             } else {
                //otherwise we set the value to the game type to the value of that attribute to tell us what game type we want to run.
                let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
                 }
         })
     }
-
+    runGame("addition");
 })
 // this is a docstring - when the function is called you can hover over the call to get a description of the
 //function without having to scroll back to find the function.
@@ -33,11 +33,21 @@ document.addEventListener('DOMContentLoaded', function() {
 /**The main game 'loop, called when the script is first loaded
  * *and after the user's answer has been processed.
  */
-
-function runGame() {
+//Pass the gameType into the function as an argument
+function runGame(gameType) {
 // Creates two random numbers beteen 1 and 25 (floor =round to whole number and plus one so that zero does not appear)
 let num1= Math.floor(Math.random() *25)+1;
 let num2= Math.floor(Math.random() *25)+1;
+
+//if the gameType is equal to addition then display our addition question
+if (gameType === "addition") {
+    displayAdditionQuestion(num1, num2);
+} else {
+    // if an error occurs it will alert the user
+    alert(`Unknown game type: ${gameType}`);
+    // throw will stop the game running and console.log this message
+    throw `Unknown game type: ${gameType}.Aborting!`;
+}
 }
 
 
@@ -56,9 +66,13 @@ function incrementScore() {
 function incrementWrongAnswer() {
 
 }
-
-function displayAdditionQuestion() {
-
+//The two arguments the function will accept are operand 1 and 2
+function displayAdditionQuestion(operand1, operand2) {
+    //Get the element that has the Id of operand1 & 2and set the text content to our number
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    //Get the element that has the Id of operator and set to plus sign
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
